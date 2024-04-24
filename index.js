5d5fde8c8958720d415ff6eb61b468df989b364a
Margarida Grilo <margaridagrilo7@gmail.com>
2024-04-24 18:56:42 +0100

    3.2 to 3.6

--- index.js
+++ index.js
@@ -30,6 +30,60 @@ app.get('/api/persons', (request, response) => {
 	response.send(persons);
 });
 
+app.post('/api/persons', (request, response) => {
+	const body = request.body;
+
+	if (!body.name || !body.number) {
+		return response.status(400).json({
+			error: 'content missing',
+		});
+	}
+
+	if (persons.find(person => person.name === body.name)) {
+		return response.status(400).json({
+			error: 'name must be unique',
+		});
+	}
+
+	const person = {
+		id: Math.random() * 1000,
+		name: body.name,
+		number: body.number,
+	};
+
+	persons = persons.concat(person);
+
+	response.json(person);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+	const person = persons.find(person => person.id === id);
+
+	if (person) {
+		console.log('Matched to person: ', person);
+		response.json(person);
+	} else {
+		console.log('No matching person found for the provided id!!');
+		response.status(404).end();
+	}
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+	persons = persons.filter(person => person.id !== id);
+
+	response.status(204).end();
+});
+
+app.get('/info', (request, response) => {
+	let info = {
+		description: `Phonebook has info for ${persons.length} people`,
+		date: new Date(), // new Date(),
+	};
+	response.send(info);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
